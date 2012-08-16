ba33c3efdfcb39d4905386b2a5b3845917b6c499
admin <admin@vivart.local>
2012-08-16 15:55:29 +0530

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by admin on 8/16/12.
+//  Copyright (c) 2012 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by admin on 8/16/12.
+//  Copyright (c) 2012 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
